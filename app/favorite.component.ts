1c71750545d5cbe73f77051ec0ce9d63427ee660
Alexandr.li <muzikant1990@gmail.com>
2017-01-06 01:13:47 +0600

    Add favorite component

---
+++ app/favorite.component.ts
@@ -0,0 +1,23 @@
+import {Component, Input, Output} from "angular2/core";
+import {EventEmitter} from "angular2/src/facade/async";
+
+@Component({
+    selector: 'favorite',
+    templateUrl: 'app/favorite.template.html',
+    styles: [`
+            .glyphicon-star{
+                color: orange;
+            }          
+`]
+})
+
+export class FavoriteComponent {
+    @Input('is-favorite') isFavorite = false;
+    @Output() change = new EventEmitter();
+
+    onClick() {
+        this.isFavorite = !this.isFavorite;
+        this.change.emit({newValue: this.isFavorite})
+    }
+
+}
