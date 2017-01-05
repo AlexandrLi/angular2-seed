4a9a0ae5ebaaf9981029b4660abb29f027c83b89
Alexandr.li <muzikant1990@gmail.com>
2017-01-06 01:19:59 +0600

    Add voter component in template

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,15 +1,27 @@
 import {Component} from "angular2/core";
 import {CoursesComponent} from "./courses.component";
 import {AuthorsComponent} from "./authors.component";
+import {AutoGrowDirective} from "./auto-grow.directive";
+import {VoterComponent} from "./voter.component";
 
 @Component({
     selector: 'my-app',
     template: `
-    <h1>Angular 2 App</h1>
-    <courses></courses>
-    <authors></authors>`,
-    directives: [CoursesComponent, AuthorsComponent]
+            <h1>Angular 2 App</h1><voter [voteCount]="post.voteCount" [myVote]="post.myVote" (vote)="onVote($event)"></voter>
+            <courses></courses>
+            <authors></authors>
+            <input type="text" autoGrow/>
+            `,
+    directives: [VoterComponent, CoursesComponent, AuthorsComponent, AutoGrowDirective]
 
 })
 export class AppComponent {
+    post = {
+        voteCount: 7,
+        myVote: 1
+    }
+
+    onVote($event) {
+        console.log($event);
+    }
 }
