a602e365e3bf564c1823bd34ff81af650a5f2ad9
Alexandr.li <muzikant1990@gmail.com>
2017-01-07 00:54:07 +0600

    Edit app component template

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,27 +1,27 @@
 import {Component} from "angular2/core";
-import {CoursesComponent} from "./courses.component";
-import {AuthorsComponent} from "./authors.component";
-import {AutoGrowDirective} from "./auto-grow.directive";
-import {VoterComponent} from "./voter.component";
+import {TweetsComponent} from "./tweets.component";
 
 @Component({
     selector: 'my-app',
     template: `
-            <h1>Angular 2 App</h1><voter [voteCount]="post.voteCount" [myVote]="post.myVote" (vote)="onVote($event)"></voter>
-            <courses></courses>
-            <authors></authors>
-            <input type="text" autoGrow/>
-            `,
-    directives: [VoterComponent, CoursesComponent, AuthorsComponent, AutoGrowDirective]
+            <div class="title"><h1><i class="glyphicon glyphicon-screenshot"></i><b>Ngitter</b></h1></div>
+            <tweets></tweets>
+        `,
+    styles: [
+        `
+            .title{
+                text-align: center;
+                vertical-align: middle;
+                line-height: 100%
+            }
+            .glyphicon-screenshot{
+                color: red;
+            }
+        `
+    ],
+    directives: [TweetsComponent]
 
 })
 export class AppComponent {
-    post = {
-        voteCount: 7,
-        myVote: 1
-    }
 
-    onVote($event) {
-        console.log($event);
-    }
 }
