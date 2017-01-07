29fc5c7fa8c6f204c8808681f9ffc17a3baf21ba
Alexandr.li <muzikant1990@gmail.com>
2017-01-08 01:09:40 +0600

    Add new components in template

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,16 +3,24 @@ import {CoursesComponent} from "./courses.component";
 import {AuthorsComponent} from "./authors.component";
 import {AutoGrowDirective} from "./auto-grow.directive";
 import {VoterComponent} from "./voter.component";
+import {ZippyComponent} from "./zippy.component";
+import {SubscribeComponent} from "./subscribe-form.component";
+import {SignupFormComponent} from "./signup-form.component";
 
 @Component({
     selector: 'my-app',
     template: `
             <h1>Angular 2 App</h1><voter [voteCount]="post.voteCount" [myVote]="post.myVote" (vote)="onVote($event)"></voter>
+            <subscribe-form></subscribe-form>
             <courses></courses>
             <authors></authors>
             <input type="text" autoGrow/>
+            <zippy title="Zippy component title">
+                <div>Zippy component body</div>
+            </zippy>
+            <signup-form></signup-form>
             `,
-    directives: [VoterComponent, CoursesComponent, AuthorsComponent, AutoGrowDirective]
+    directives: [VoterComponent, CoursesComponent, AuthorsComponent, AutoGrowDirective, ZippyComponent, SubscribeComponent, SignupFormComponent]
 
 })
 export class AppComponent {
