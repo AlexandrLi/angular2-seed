f57b6d118659456394358c9dc7a2e4156cb8211f
Alexandr.li <muzikant1990@gmail.com>
2017-01-09 23:41:09 +0600

    Edit app template, implement onInit interface

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from "angular2/core";
+import {Component, OnInit} from "angular2/core";
 import {CoursesComponent} from "./courses.component";
 import {AuthorsComponent} from "./authors.component";
 import {AutoGrowDirective} from "./auto-grow.directive";
@@ -8,10 +8,15 @@ import {SubscribeComponent} from "./subscribe-form.component";
 import {SignupFormComponent} from "./signup-form.component";
 import {PassChangeFormComponent} from "./passchange-form.component";
 import {ObservablesComponent} from "./observables.component";
+import {PostService} from "./post.service";
+import {HTTP_PROVIDERS} from "angular2/http";
 
 @Component({
     selector: 'my-app',
     template: `
+        <div *ngIf="isLoading">
+            <i class="fa fa-spinner fa-spin fa-3x"></i>
+        </div>
             <h1>Angular 2 App</h1><voter [voteCount]="post.voteCount" [myVote]="post.myVote" (vote)="onVote($event)"></voter>
             <subscribe-form></subscribe-form>
             <courses></courses>
@@ -23,14 +28,29 @@ import {ObservablesComponent} from "./observables.component";
             <signup-form></signup-form>
             <passchange-form></passchange-form>
             `,
-    directives: [VoterComponent, CoursesComponent, AuthorsComponent, AutoGrowDirective, ZippyComponent, SubscribeComponent, SignupFormComponent, PassChangeFormComponent, ObservablesComponent]
+    directives: [VoterComponent, CoursesComponent, AuthorsComponent, AutoGrowDirective, ZippyComponent, SubscribeComponent, SignupFormComponent, PassChangeFormComponent, ObservablesComponent],
+    providers: [PostService, HTTP_PROVIDERS]
 
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+    isLoading = true;
     post = {
         voteCount: 7,
         myVote: 1
     }
+
+    constructor(private _postService: PostService) {
+    }
+
+    ngOnInit() {
+        this._postService.getPosts()
+            .delay(2000)
+            .subscribe(posts => {
+                this.isLoading = false;
+                console.log(posts)
+            });
+    }
+
 
     onVote($event) {
         console.log($event);
