983b99b1440150a8097a50d660905a65a5bfda3d
Alexandr.li <muzikant1990@gmail.com>
2017-01-13 01:05:07 +0600

    Add navbar component

---
+++ app/navbar.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from "angular2/core";
+import {ROUTER_DIRECTIVES} from "angular2/router";
+
+@Component({
+    selector: 'navbar',
+    templateUrl: 'app/navbar.component.html',
+    directives: [ROUTER_DIRECTIVES]
+})
+export class NavbarComponent implements OnInit {
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+
+}
