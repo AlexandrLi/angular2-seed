112a5259b2064728a32c41ff139376f60c1cfa43
Alexandr.li <muzikant1990@gmail.com>
2017-01-13 01:05:21 +0600

    Add posts component

---
+++ app/posts.component.ts
@@ -0,0 +1,14 @@
+import {Component, OnInit} from "angular2/core";
+
+@Component({
+    selector: 'posts',
+    template: '<h2>Posts</h2>'
+})
+export class PostsComponent implements OnInit {
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+
+}
