1afe21baebffda707942a63b04066f19bd095d42
Alexandr.li <muzikant1990@gmail.com>
2017-01-13 01:03:25 +0600

    Add home component

---
+++ app/home.component.ts
@@ -0,0 +1,14 @@
+import {Component, OnInit} from "angular2/core";
+
+@Component({
+    selector: 'home',
+    template: '<h2>Home</h2>'
+})
+export class HomeComponent implements OnInit {
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+
+}
