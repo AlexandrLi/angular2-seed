30d1af26b9dd1eae9ea092b0a3897ccc3aa3cab3
Alexandr.li <muzikant1990@gmail.com>
2017-01-13 01:05:35 +0600

    Add users component

---
+++ app/users.component.ts
@@ -0,0 +1,14 @@
+import {Component, OnInit} from "angular2/core";
+
+@Component({
+    selector: 'users',
+    template: '<h2>Users</h2>'
+})
+export class UsersComponent implements OnInit {
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+
+}
