52e2608c9245b92b5e3b915a9a9d4f6099d4f517
Alexandr.li <muzikant1990@gmail.com>
2017-01-13 01:07:03 +0600

    Add route config

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,25 +1,25 @@
 import {Component} from "angular2/core";
 import {RouteConfig, ROUTER_DIRECTIVES} from "angular2/router";
-import {AlbumsComponent} from "./albums.component";
-import {ContactComponent} from "./contact.component";
-import {AlbumComponent} from "./album.component";
-import {ArchivesComponent} from "./archives.component";
-import {ArchiveComponent} from "./archive.component";
+import {HomeComponent} from "./home.component";
+import {NavbarComponent} from "./navbar.component";
+import {UsersComponent} from "./users.component";
+import {PostsComponent} from "./posts.component";
 
 @RouteConfig([
-    {path: '/albums', name: 'Albums', component: AlbumsComponent, useAsDefault: true},
-    {path: '/album/:id', name: 'Album', component: AlbumComponent},
-    {path: '/archives', name: 'Archives', component: ArchivesComponent},
-    {path: '/archive/:year/:month', name: 'Archive', component: ArchiveComponent},
-    {path: '/archive/*other', name: 'OtherArchive', redirectTo:['Archives']},
-    {path: '/contact', name: 'Contact', component: ContactComponent},
-    {path: '/*other', name: 'Other', redirectTo: ['Albums']}
+    {path: '/', name: 'Home', component: HomeComponent, useAsDefault: true},
+    {path: '/users', name: 'Users', component: UsersComponent},
+    {path: '/posts', name: 'Posts', component: PostsComponent},
+    {path: '/*other', name: 'Other', redirectTo: ['Home']}
 ])
 
 @Component({
     selector: 'my-app',
-    templateUrl: '/app/app.component.html',
-    directives: [ROUTER_DIRECTIVES]
+    template: `<navbar></navbar>
+                <div class="container">
+                    <router-outlet></router-outlet>
+                </div>
+`,
+    directives: [NavbarComponent, ROUTER_DIRECTIVES]
 })
 export class AppComponent {
 }
