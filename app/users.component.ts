a299523f69291af2430382d6b0e7b8759d812910
Alexandr.li <muzikant1990@gmail.com>
2017-01-15 00:38:47 +0600

    Add delete user mechanism

--- app/users.component.ts
+++ app/users.component.ts
@@ -1,18 +1,18 @@
 import {Component, OnInit} from "angular2/core";
-import {UsersService} from "./users.service";
 import {ROUTER_DIRECTIVES} from "angular2/router";
+import {UserService} from "./user.service";
 
 @Component({
     selector: 'users',
     templateUrl: 'app/users.component.html',
     directives: [ROUTER_DIRECTIVES],
-    providers: [UsersService]
+    providers: [UserService]
 })
 export class UsersComponent implements OnInit {
     users;
 
-    constructor(private _usersService: UsersService) {
-        this._usersService
+    constructor(private _userService: UserService) {
+        this._userService
             .getUsers()
             .subscribe(users => this.users = users);
     }
@@ -20,4 +20,17 @@ export class UsersComponent implements OnInit {
     ngOnInit() {
     }
 
+    deleteUser(user) {
+        if (confirm("Are you sure, you want to delete " + user.name + "?")) {
+            var index = this.users.indexOf(user);
+            this.users.splice(index, 1);
+            this._userService.deleteUser(user.id)
+                .subscribe(null,
+                    err => {
+                        alert("Could not delete the user.")
+                        this.users.splice(index, 0, user);
+                    })
+        }
+
+    }
 }
