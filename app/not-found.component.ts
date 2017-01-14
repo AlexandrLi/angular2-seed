899acc0a40b3b2fb32550ae6074333cab2cb4925
Alexandr.li <muzikant1990@gmail.com>
2017-01-15 00:39:03 +0600

    Add not-found component

---
+++ app/not-found.component.ts
@@ -0,0 +1,14 @@
+import {Component, OnInit} from "angular2/core";
+
+@Component({
+    selector: 'posts',
+    template: '<h2>Not Found</h2>'
+})
+export class NotFoundComponent implements OnInit {
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+
+}
