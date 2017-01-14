339a502a6cb2f97c3b2746446702ae31b4be9f33
Alexandr.li <muzikant1990@gmail.com>
2017-01-15 00:41:06 +0600

    Apply form fields two-way binding

--- app/user-form.component.ts
+++ app/user-form.component.ts
@@ -2,30 +2,72 @@ import {Component, OnInit} from "angular2/core";
 import {FormBuilder} from "angular2/src/common/forms/form_builder";
 import {Validators} from "angular2/src/common/forms/validators";
 import {BasicValidators} from "./basicValidators";
+import {CanDeactivate, Router} from "angular2/router";
+import {ControlGroup} from "angular2/src/common/forms/model";
+import {UserService} from "./user.service";
+import {RouteParams} from "angular2/src/router/instruction";
+import {User} from "./user";
 
 @Component({
     selector: 'user-form',
     templateUrl: 'app/user-form.component.html',
-    providers: [FormBuilder]
+    providers: [FormBuilder, UserService]
 })
-export class UserFormComponent implements OnInit {
-    form;
+export class UserFormComponent implements OnInit, CanDeactivate {
+    form: ControlGroup;
+    title: string;
+    user = new User();
+    isSaving = false;
 
-    constructor(fb: FormBuilder) {
+    constructor(fb: FormBuilder,
+                private _userService: UserService,
+                private _router: Router,
+                private _routeParams: RouteParams) {
         this.form = fb.group({
             name: ['', Validators.required],
             email: ['', Validators.compose([Validators.required, BasicValidators.email])],
-            phone: [],
+            phone: [''],
             address: fb.group({
-                street: [],
-                suite: [],
-                city: [],
-                zipcode: []
+                street: [''],
+                suite: [''],
+                city: [''],
+                zipcode: ['']
             })
         });
     }
 
     ngOnInit() {
+        var id = this._routeParams.get('id');
+        this.title = id ? "Edit User" : "Add User";
+        if (!id) {
+            return;
+        }
+        this._userService.getUser(id)
+            .subscribe(user => this.user = user,
+                response => {
+                    if (response.status == 404) {
+                        this._router.navigate(['NotFound']);
+                    }
+                });
     }
 
+    routerCanDeactivate(next, previous) {
+        if (this.form.dirty && !this.isSaving) {
+            return confirm("You lost all filled data, are you sure?")
+        }
+        return true;
+    }
+
+    saveUser() {
+        var result;
+        if (this.user.id)
+            result = this._userService.updateUser(this.user);
+        else
+            result = this._userService.postUser(this.user);
+
+        result.subscribe(result => {
+            this.isSaving = true;
+            this._router.navigate(['Users']);
+        });
+    }
 }
