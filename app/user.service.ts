c02f302c95927b094fe614401329dfb65e7749df
Alexandr.li <muzikant1990@gmail.com>
2017-01-15 00:39:47 +0600

    Add user service with CRUD operations

---
+++ app/user.service.ts
@@ -0,0 +1,40 @@
+import {Http} from "angular2/src/http/http";
+import {Injectable} from "angular2/src/core/di/decorators";
+import "rxjs/add/operator/map";
+
+@Injectable()
+export class UserService {
+    private _url = 'https://jsonplaceholder.typicode.com/users/';
+
+    constructor(private _http: Http) {
+    }
+
+    postUser(user) {
+        return this._http.post(this._url, JSON.stringify(user))
+            .map(user => user.json());
+    }
+
+    getUser(userId) {
+        return this._http.get(this.getUserUrl(userId))
+            .map(user => user.json());
+    }
+
+    updateUser(user) {
+        return this._http.put(this.getUserUrl(user.id), JSON.stringify(user))
+            .map(user => user.json());
+    }
+
+    getUsers() {
+        return this._http.get(this._url)
+            .map(users => users.json());
+    }
+
+    deleteUser(userId) {
+        return this._http.delete(this.getUserUrl(userId))
+            .map(user => user.json());
+    }
+
+    getUserUrl(userId) {
+        return this._url + userId;
+    }
+}
