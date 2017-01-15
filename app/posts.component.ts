c9e2b09677bbf79b2a824e6e6fce158fffbff2fc
Alexandr.li <muzikant1990@gmail.com>
2017-01-16 00:33:31 +0600

    Implement post component

--- app/posts.component.ts
+++ app/posts.component.ts
@@ -1,14 +1,54 @@
-import {Component, OnInit} from "angular2/core";
+import {Component} from "angular2/core";
+import {PostService} from "./post.service";
+import {SpinnerComponent} from "./spinner.component";
+import {UserService} from "./user.service";
 
 @Component({
     selector: 'posts',
-    template: '<h2>Posts</h2>'
+    templateUrl: "app/posts.component.html",
+    providers: [PostService, UserService],
+    directives: [SpinnerComponent]
 })
-export class PostsComponent implements OnInit {
-    constructor() {
+export class PostsComponent {
+    isLoading = true;
+    posts = [];
+    comments = [];
+    users = [];
+    currentPost;
+
+    constructor(private _postService: PostService,
+                private _userService: UserService) {
+        this.loadPosts();
+        this.loadUsers();
     }
 
-    ngOnInit() {
+    select(post) {
+        this.currentPost = post;
+        this.isLoading = true;
+        this._postService.getPostComments(this.currentPost.id)
+            .subscribe(comments => {
+                this.isLoading = false;
+                this.comments = comments;
+            })
+    }
+
+    private loadUsers() {
+        this._userService.getUsers()
+            .subscribe(users => this.users = users);
+    }
+
+    private loadPosts(filter?) {
+        this._postService.getPosts(filter)
+            .subscribe(posts => {
+                this.isLoading = false;
+                this.posts = posts;
+            });
+    }
+
+    reloadPosts(filter) {
+        this.isLoading = true;
+        this.currentPost = null;
+        this.loadPosts(filter);
     }
 
 }
